1c12ae37899987d2f0530ee77b01d1aa86be4475
sundharp <sundharp@SIT-DT-001.SIT.LOCAL>
2020-02-28 12:33:07 +0530

    First setup

---
+++ Jenkin/src/JenkinsTutorial/Class01.java
@@ -0,0 +1,17 @@
+package JenkinsTutorial;
+
+public class Class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("S");
+		System.out.println("U");
+		System.out.println("N");
+		System.out.println("D");
+		System.out.println("H");
+		System.out.println("A");
+		System.out.println("R");
+	}
+
+}
---
+++ Jenkin/src/JenkinsTutorial/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author sundharp
+ *
+ */
+package JenkinsTutorial;
